fcbb8fa995c9187a9d5cb8db396e740cb1346fe1
Anirban Dutta <meanirbandutta22@gmail.com>
2024-08-17 20:45:03 +0530

    basic boiler plate updated

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Hello this is backend code for a MERN based chat application");
+//external imports
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+//internal imports
+const connectDB = require("./db/config");
+const app = require("./app");
+
+connectDB();
+app.listen(process.env.PORT, () => {
+  console.log(`app is running on ${process.env.PORT}`);
+});
